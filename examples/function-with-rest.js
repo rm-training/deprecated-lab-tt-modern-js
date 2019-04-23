d6e4b6b510b3ac3740fdd339647dcd528a3dcd6d
Ryan Morris <mr.morris@gmail.com>
2019-04-23 07:42:30 -0400

    solve rest

--- examples/function-with-rest.js
+++ examples/function-with-rest.js
@@ -2,13 +2,7 @@
 // takes any number of arguments
 // returns their sum
 // ex: adder(1,2,3,4,5,6);
-function adder() {
-	// prior to es2015....
-	// we'd convert arguments, which is array-like, to a real array
-	var args = Array.prototype.slice.call(arguments);
-
-	console.log(args);
-
+function adder(...args) {
 	return args.reduce((acc, curr) => {
 		return acc + curr;
 	}, 0);
