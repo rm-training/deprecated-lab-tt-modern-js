e8350be750736a62422a79ceabea156088b74819
Ryan Morris <mr.morris@gmail.com>
2019-04-23 08:11:08 -0400

    Literals

--- examples/template-literals.js
+++ examples/template-literals.js
@@ -2,12 +2,15 @@ const name = 'Abe';
 const age = 101;
 
 console.log(
-  `${name} is ${age} years old`,
+  name + ' is ' + age + ' years old'
 );
 
-function convertStringAge(str, name, age) {
-  const ageString = age > 100 ? 'super old' : age;
-  return `${name}${str[1]}${ageString}`;
-}
+// We can also run our strings through a function
+// using a tag function
+//
+// function convertStringAge(str, name, age) {
+//   const ageString = age > 100 ? 'super old' : age;
+//   return `${name}${str[1]}${ageString}`;
+// }
 
-console.log(convertStringAge`${name} is ${age} years old`);
+// console.log(convertStringAge`${name} is ${age} years old`);
